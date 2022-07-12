86fdbd1f6a5446bd5060641ac00f85eaf6dcb4f7
micacalvo19 <107216867+micacalvo19@users.noreply.github.com>
2022-07-12 11:50:20 -0300

    Delete MVTCalvo directory

--- MVTCalvo/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Familiar(models.Model):
-    nombre = models.CharField(max_length=50)
-    edad = models.IntegerField()
-    nacimiento = models.DateField()
