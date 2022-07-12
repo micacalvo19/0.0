b56258f7933e4f6e8cb433f43220fc9852290a54
micacalvo19 <107216867+micacalvo19@users.noreply.github.com>
2022-07-12 11:53:05 -0300

    Delete ProyectoFinal/ProyectoFinal1 directory

--- ProyectoFinal/ProyectoFinal1/App1/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from App1.models import *
-# Register your models here.
-admin.site.register(Formulario)
--- ProyectoFinal/ProyectoFinal1/App1/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class FormularioForm(forms.Form):
-    nombre = forms.CharField(max_length=50)
-    apellido = forms.CharField(max_length=50)
-    numero_de_socio = forms.IntegerField()
