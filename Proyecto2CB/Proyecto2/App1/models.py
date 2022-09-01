c6b2702937acc6e91e3fcdbf72e56c0409a0343e
micacalvo19 <107216867+micacalvo19@users.noreply.github.com>
2022-08-31 22:55:22 -0300

    Delete Proyecto2CB/Proyecto2 directory

--- Proyecto2CB/Proyecto2/App1/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Avatar(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
-    imagen= models.ImageField(upload_to='avatares', null=True, blank=True)
-     
-
--- Proyecto2CB/Proyecto2/AppMensajes/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class MensajeFormulario(forms.Form):
-    cuerpo= forms.CharField(max_length=10000)
